7b2c271aadfdd3a7e5aa17d36a036becd7fc768e
devMartin <mwendadev@gmail.com>
2022-12-08 12:22:57 -0800

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * puts - prints string
+ * Return: 0 if successful
+*/
+int main()
+{
+	char str[] = "\"Programming is like building a multilingual puzzle"
+	puts (str);
+	return 0;
+}
