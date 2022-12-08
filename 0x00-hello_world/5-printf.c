2a1128ec14c9378486f6e1009fc4c0652dba7f40
devMartin <mwendadev@gmail.com>
2022-12-08 12:50:46 -0800

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * printf - print string
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	char myStr[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", myStr);
+
+	return (0);
+}
+
