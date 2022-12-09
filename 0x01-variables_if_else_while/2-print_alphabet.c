53cd2eaa5d65aeb1ae74da4fddd422fddc7631a0
devMartin <mwendadev@gmail.com>
2022-12-09 06:28:10 -0800

    putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 if executed successfully
+*/
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n')
+	return (0);
+}
