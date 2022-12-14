46cf239350229ddbc41b6191f6348e82a67b3238
devMartin <mwendadev@gmail.com>
2022-12-14 00:50:29 -0800

    _putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
  *main - entry point
  *_putchar - writes character c to stdout
@@ -10,7 +10,7 @@ int main(void)
 	char c[8] = "_putchar";
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		_putchar(c[i]);
 
