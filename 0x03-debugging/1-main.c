678c0fd004a78471bf18c51cbad311b8e8903320
devMartin <mwendadev@gmail.com>
2022-12-15 04:52:29 -0800

    Debugging task one

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)/*this while loop does not have an increment*/
-	{
-		putchar(i);
-	}
+	/*while (i < 10)*/
+	/*{*/
+		/*putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
