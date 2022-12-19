1b75381427d74a164a5a4011f5d2b66dafb00858
devMartin <mwendadev@gmail.com>
2022-12-19 12:11:25 -0800

    reverses string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,8 +5,25 @@
 */
 void rev_string(char *s)
 {
-	for (*s == '\0'; *s >= 0; s--)
+	int i, j, k;
+	char *a, b;
+
+	a = s;
+
+	while (s[j] != '\0')
 	{
-		_putchar(*s);
+		j++;
 	}
+	for (k = 1; k < j; k++)
+	{
+		a++;
+	}
+	for (i = 0; i < (j / 2); i++)
+	{
+		b = s[i];
+		s[i] = *a;
+		*a = b;
+		a--;
+	}
+
 }
